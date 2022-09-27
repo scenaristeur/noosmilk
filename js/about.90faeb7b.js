3c2b1099d6640f6ac20f4ce76759b1a5525cc5b4
scenaristeur <scenaristeur@gmail.com>
2022-09-27 14:59:01 +0200

    Initial dist subtree commit

---
+++ js/about.90faeb7b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunknoosmilk"]=self["webpackChunknoosmilk"]||[]).push([[443],{7381:function(n,s,u){u.r(s),u.d(s,{default:function(){return f}});var t=u(3396);const a={class:"about"},e=(0,t._)("h1",null,"This is an about page",-1),o=[e];function r(n,s){return(0,t.wg)(),(0,t.iD)("div",a,o)}var i=u(89);const c={},l=(0,i.Z)(c,[["render",r]]);var f=l}}]);
+//# sourceMappingURL=about.90faeb7b.js.map
