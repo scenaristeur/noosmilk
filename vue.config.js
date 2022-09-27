941629065773fc30fc52876b8adfa5afc0250138
scenaristeur <scenaristeur@gmail.com>
2022-09-27 15:05:49 +0200

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,35 @@
+const publicPath = process.env.NODE_ENV === 'production' ? '/noosmilk/' : '/'
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: publicPath,
+  pwa: {
+    name: 'Noos Milk',
+    // themeColor: '#4dbab5',
+    // msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      share_target: {
+        action: publicPath+'share',
+        method: "GET",
+        enctype: "application/x-www-form-urlencoded",
+        params: {
+          title: "title",
+          text: "text",
+          url: "url"
+        }
+      },
+    },
+
+
+    // configure the workbox plugin
+    /*  workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+    // swSrc is required in InjectManifest mode.
+    swSrc: 'src/registerServiceWorker.js',
+    // ...other Workbox options...
+  }*/
+},
+transpileDependencies: true
 })
